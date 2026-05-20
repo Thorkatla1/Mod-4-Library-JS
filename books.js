71db4beebb754a19233814431711eff65ecc72d3
Thorkatla1 <thorkatla@yahoo.com>
2026-05-20 09:55:14 -0400

    finished price

--- books.js
+++ books.js
@@ -1,12 +1,17 @@
 function renderBooks(filter) {
   const booksWrapper = document.querySelector(".books");
-
   const books = getBooks();
 
   if (filter === "LOW_TO_HIGH") {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
-  } else if (filter === "HIGH__TO__LOW") {
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
+    books.sort(
+      (a, b) =>
+        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice),
+    );
+  } else if (filter === "HIGH_TO_LOW") {
+    books.sort(
+      (a, b) =>
+        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice),
+    );
   } else if (filter === "RATING") {
     books.sort((a, b) => b.rating - a.rating);
   }
@@ -18,18 +23,26 @@ function renderBooks(filter) {
     <img class="book__img" src="${book.url}" alt="">
   </figure>
   <div class="book__title">
-  ${book.title}
+    ${book.title}
   </div>
-  <div class="book__ratings">${ratingsHTML(book.rating)}
-  </div>
-  <div class="book__price">
-    <span class="">$${book.originalPrice.toFixed(2)}</span>
-  </div>
-</div>`;
+  <div class="book__ratings">
+    ${ratingsHTML(book.rating)}
+  </div>  
+   <div class="book__price">
+  ${priceHTML(book.originalPrice, book.salePrice)}
+    </div>
+  </div>`;
     })
     .join("");
 
   booksWrapper.innerHTML = booksHtml;
+}
+
+function priceHTML(originalPrice, salePrice) {
+  if (!salePrice) {
+    return `$${originalPrice.toFixed(2)}`;
+  }
+  return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span> $${salePrice.toFixed(2)}`;
 }
 
 function ratingsHTML(rating) {
